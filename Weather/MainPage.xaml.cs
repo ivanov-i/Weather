949d14059916b00d6abd9d06e0bd0ab0eece3846
ivanov-i <ivanov.i@live.com>
2016-01-17 09:26:54 +0600

    TIle

---
+++ Weather/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Weather
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            UpdateTile("The Text");
+        }
+
+        private static void UpdateTile(string infoString)
+        {
+            // create the instance of Tile Updater, which enables you to change the appearance of the calling app's tile
+            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+
+            // enables the tile to queue up to five notifications
+            updater.EnableNotificationQueue(true);
+            updater.Clear();
+
+            // get the XML content of one of the predefined tile templates, so that, you can customize it
+            var tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150Text04);
+
+            tileXml.GetElementsByTagName("text")[0].InnerText = infoString;
+
+            // Create a new tile notification. 
+            updater.Update(new TileNotification(tileXml));
+        }
+    }
+}
